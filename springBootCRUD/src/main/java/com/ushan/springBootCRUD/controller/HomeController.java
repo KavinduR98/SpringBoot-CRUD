4337bb38773cb36c42d03e9393e9747f46d24f1c
@kavinduushan07 <kavinduushan07@gmail.com>
2025-05-05 15:46:58 +0530

    implement save-customer

--- springBootCRUD/src/main/java/com/ushan/springBootCRUD/controller/HomeController.java
+++ springBootCRUD/src/main/java/com/ushan/springBootCRUD/controller/HomeController.java
@@ -5,6 +5,8 @@ import com.ushan.springBootCRUD.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -23,6 +25,18 @@ public class HomeController {
         List<Customer> customerList = customerService.getAllCustomers();
         model.addAttribute("customerList", customerList);
         return "home";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model){
+        model.addAttribute("customer", new Customer());
+        return "create";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute("customer") Customer customer, Model model){
+        customerService.save(customer);
+        return "redirect:/";
     }
 
 }
